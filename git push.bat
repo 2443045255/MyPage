1fb015eb7a825be505554ff966bfe5b41b64301e
Rxaser <2443045255@qq.com93034610+2443045255@users.noreply.github.com>
2024-10-09 00:48:33 +0800

    2024/10/09 周三 0:48:33.21

--- git push.bat
+++ git push.bat
@@ -1,4 +1,4 @@
-git remote add origin https://github.com/2443045255/MyPage.git
+@REM git remote add origin https://github.com/2443045255/MyPage.git
 git add .
 git commit -m "%date%%time%"
 git branch -m main
