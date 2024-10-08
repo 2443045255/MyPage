f168a21870264d458693e381bcdadb06b9a89a69
Rxaser <2443045255@qq.com93034610+2443045255@users.noreply.github.com>
2024-10-09 01:15:59 +0800

    2024/10/09 周三 1:15:59.93

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
-    base: '/MyPage/dist/',
-  },
-  
+  // base: '/MyPage/dist/',
+
   plugins: [
     vue(),
   ],
