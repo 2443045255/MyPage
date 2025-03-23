21ec6367dc9003adf8d9210b8ebaeb4affb8ea6d
2443045255 <93034610+2443045255@users.noreply.github.com>
2025-03-24 00:16:53 +0800

    2025/03/24 周一 0:16:53.68

--- src/views/PublicView.vue
+++ src/views/PublicView.vue
@@ -1,7 +1,7 @@
 <template>
-    <main>
-        <h1>这是资源页</h1>
-        <h2>暂无内容</h2>
-        <RouterLink class="likeBtn1 mt_20px" to="/chat" replace>进入聊天室</RouterLink>
-    </main>
+  <main>
+    <h1>这是资源页</h1>
+    <h2>暂无内容</h2>
+    <RouterLink class="likeBtn1 mt_20px" to="/chat" replace>进入聊天室</RouterLink>
+  </main>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: '/MyPage/dist/',
-    base: '/',
-    plugins: [
-        vue(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
+  // base: '/MyPage/dist/',
+  base: '/',
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
 
 })
